b5babc82e77f76f8a7a994f3b79d49628e984b66
ROMAN PARZER <roman.parzer@tuwien.ac.at>
2023-10-11 15:24:10 +0200

    small shange (a instead of m for number of active vars)

--- inst/creata_example_data_SPAR.R
+++ inst/creata_example_data_SPAR.R
@@ -2,14 +2,14 @@
 n <- 200
 p <- 2000
 ntest <- 100
-m <- 100
+a <- 100
 snr <- 10
 rho <- 0.5
 mu <- 1
 
 beta <- numeric(p)
 set.seed(1234)
-beta[1:m] <- sample(c(-3:3)[-4],m,replace = TRUE)
+beta[1:a] <- sample(c(-3:3)[-4],a,replace = TRUE)
 x <- sqrt(rho)*matrix(rep(rnorm((n+ntest),0,1),p),n+ntest,p) + sqrt(1-rho)*matrix(rnorm((n+ntest)*p,0,1),n+ntest,p)
 bSb <- rho*sum(beta)^2 + (1-rho)*sum(beta^2)
 sigma2 <- bSb/snr
