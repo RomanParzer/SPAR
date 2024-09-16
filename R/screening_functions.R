c6b1dddd7b4519e0fcda3c03841dc2f5051e07bc
ROMAN PARZER <roman.parzer@tuwien.ac.at>
2024-09-16 13:03:05 +0200

    screen coef Ridge lambda dep on family

--- R/screening_functions.R
+++ R/screening_functions.R
@@ -4,11 +4,14 @@ screening_ridge_lambda0 <- function(z, yz, family) {
   tmp_sc <- apply(z,2,function(col)sqrt(var(col)*(n-1)/n))
   z2 <- scale(z,center=colMeans(z),scale=tmp_sc)
   lam_max <- 1000 * max(abs(t(yz)%*%z2[,tmp_sc>0]))/n*family$mu.eta(family$linkfun(mean(yz)))/family$variance(mean(yz))
-
-  glmnet_res <- glmnet::glmnet(x=z, y=yz, family = family, alpha=0,lambda.min.ratio = min(0.01,1e-7/n / lam_max))
-  lam <- min(glmnet_res$lambda)
+  if (family$family=="gaussian") {
+    dev.ratio_cutoff <- 0.999
+  } else {
+    dev.ratio_cutoff <- 0.8
+  }
+  glmnet_res <- glmnet::glmnet(x=z, y=yz, family = family, alpha=0,lambda.min.ratio = min(0.01,1e-4 / lam_max))
+  lam <- min(glmnet_res$lambda[glmnet_res$dev.ratio<=dev.ratio_cutoff])
   scr_coef <- coef(glmnet_res,s=lam)[-1]
-
   scr_coef
 }
 
@@ -23,7 +26,7 @@ screening_marglik <- function(z, yz, family) {
 
 screening_corr <- function(z, yz, family) {
   n <- nrow(z)
-  if (family$family != "gaussian") warning("Screening based on correlation is typically employed for Gaussian variables.")
+  if (family != "gaussian") warning("Screening based on correlation is typically employed for Gaussian variables.")
   scr_coef <- sapply(seq_len(NCOL(z)), function(i) cor(yz, z[,i]))
   scr_coef
 }
